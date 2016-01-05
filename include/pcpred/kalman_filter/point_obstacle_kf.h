6d1c1adcc64ef65088046bb9ee9eaef6ec61c18d
jaesungp <jaesungp@cs.unc.edu>
2016-01-05 17:05:32 -0500

    start code for point obstacle kalman filter

--- include/pcpred/kalman_filter/point_obstacle_kf.h
+++ include/pcpred/kalman_filter/point_obstacle_kf.h
@@ -2,13 +2,42 @@
 #define POINT_OBSTACLE_KF_H
 
 
+#include <Eigen/Dense>
+
+
 namespace pcpred
 {
+
+class GaussianDistribution
+{
+public:
+
+    GaussianDistribution(int dimension);
+
+private:
+
+    const int dimension_;
+
+    Eigen::VectorXd mu_;
+    Eigen::MatrixXd sigma_;
+};
+
 
 class PointObstacleKalmanFilter
 {
 public:
+
 private:
+
+    // timestep
+    double timestep_;
+
+    // kalman filter matrices
+    // x_t = A x_{t-1} + B u_t + e_t
+    // z_t = C x_t + d_t
+    Eigen::Matrix4d A_;
+    Eigen::MatrixXd B_;
+    Eigen::MatrixXd C_;
 };
 
 }
--- src/kalman_filter/point_obstacle_kf.cpp
+++ src/kalman_filter/point_obstacle_kf.cpp
@@ -1,3 +1,11 @@
 #include <pcpred/kalman_filter/point_obstacle_kf.h>
 
 using namespace pcpred;
+
+
+GaussianDistribution::GaussianDistribution(int dimension)
+    : dimension_(dimension)
+{
+    mu_ = Eigen::Vector3d(dimension);
+    sigma_ = Eigen::MatrixXd(dimension, dimension);
+}
