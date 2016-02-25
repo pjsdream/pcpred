3cc9e6296110314a7d87396fcdb3d0208d9729d2
jaesungp <jaesungp@cs.unc.edu>
2016-02-25 10:56:46 -0500

    new human features

--- include/pcpred/feature/human_motion_feature.h
+++ include/pcpred/feature/human_motion_feature.h
@@ -4,9 +4,6 @@
 
 #include <Eigen/Dense>
 
-#include <pcpred/feature/hermite_curve.h>
-#include <pcpred/feature/stream_curve_fitting.h>
-
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
@@ -22,19 +19,13 @@ namespace pcpred
 class HumanMotionFeature
 {
 public:
-    enum FeatureType
-    {
-        FEATURE_TYPE_ABSOLUTE_POSITION = 0,
-    };
-
-public:
 
     HumanMotionFeature();
 
-    void setDefaultJointNames();
-    void setJointNames(const std::vector<std::string>& joints);
-    void setCurveShape(int num_pieces, double duration);
-    void addLink(const std::string& joint1, const std::string& joint2);
+    void loadHumanJoints(const std::string& filename);
+    void clear();
+
+    void addFrame(const Eigen::VectorXd& column);
 
     inline int numJoints()
     {
@@ -46,38 +37,19 @@ public:
         return joint_names_[i];
     }
 
-    void clear();
-    void observe(const std::string& joint_name, double time, const Eigen::Vector3d& joint_position);
-
-    int featureSize(FeatureType feature_type);
-    Eigen::VectorXd toFeature(FeatureType feature_type);
-
-    // output encode/decode
-    int encodingSize();
-    Eigen::VectorXd encode();
-    void decode(const Eigen::VectorXd& code);
+    Eigen::MatrixXd feature();
 
     void setVisualizerTopic(const std::string& topic);
-    void visualizeHumanMotion();
+    void visualizeHumanMotion(const std::string& ns = "human");
+    void visualizeHumanMotion(int start, int end, const std::string& ns = "human");
 
 private:
-
-    int featureSizeAbsolutePosition();
-    Eigen::VectorXd toFeatureAbsolutePosition();
-
-    void appendTraceMarkers(double size, const std_msgs::ColorRGBA& color0, const std_msgs::ColorRGBA& color1, visualization_msgs::MarkerArray& marker_array);
-    void appendJointMarkers(double time, double size, const std_msgs::ColorRGBA& color, visualization_msgs::MarkerArray& marker_array);
-    void appendSkeletonMarkers(double time, double size, const std_msgs::ColorRGBA& color, visualization_msgs::MarkerArray& marker_array);
 
     std::vector<std::string> joint_names_;
     std::map<std::string, int> map_joint_to_index_;
     std::vector<std::pair<int, int> > links_;
 
-    int num_pieces_;
-    double duration_;
-
-    std::vector<StreamCurveFitting> streams_;
-    std::vector<HermiteCurve> curves_;
+    Eigen::MatrixXd feature_;
 
     ros::Publisher publisher_;
 };
--- src/feature/human_motion_feature.cpp
+++ src/feature/human_motion_feature.cpp
@@ -1,5 +1,7 @@
 #include <pcpred/feature/human_motion_feature.h>
 
+#include <utility>
+
 #include <stdio.h>
 
 using namespace pcpred;
@@ -8,232 +10,99 @@ using namespace pcpred;
 HumanMotionFeature::HumanMotionFeature()
 {
     setVisualizerTopic("human_motion");
-
-    setCurveShape(5, 1.0);
-
-    setDefaultJointNames();
 }
 
-void HumanMotionFeature::setVisualizerTopic(const std::string &topic)
+void HumanMotionFeature::loadHumanJoints(const std::string& filename)
 {
-    ros::NodeHandle nh;
-    publisher_.shutdown();
-    publisher_ = nh.advertise<visualization_msgs::MarkerArray>(topic, 100);
-}
+    clear();
 
-void HumanMotionFeature::setDefaultJointNames()
-{
-    static std::vector<std::string> default_joints;
-    static bool default_joints_initialized = false;
+    FILE* fp = fopen(filename.c_str(), "r");
 
-    if (!default_joints_initialized)
+    char name[128];
+
+    int num_joints;
+    fscanf(fp, "%d", &num_joints);
+    for (int i=0; i<num_joints; i++)
     {
-        default_joints_initialized = true;
-
-        default_joints.push_back("head_1");
-        default_joints.push_back("left_elbow_1");
-        default_joints.push_back("left_foot_1");
-        default_joints.push_back("left_hand_1");
-        default_joints.push_back("left_hip_1");
-        default_joints.push_back("left_knee_1");
-        default_joints.push_back("left_shoulder_1");
-        default_joints.push_back("neck_1");
-        default_joints.push_back("torso_1");
-        default_joints.push_back("right_elbow_1");
-        default_joints.push_back("right_foot_1");
-        default_joints.push_back("right_hand_1");
-        default_joints.push_back("right_hip_1");
-        default_joints.push_back("right_knee_1");
-        default_joints.push_back("right_shoulder_1");
+        fscanf(fp, "%s", name);
+        joint_names_.push_back(name);
+        map_joint_to_index_[name] = i;
     }
 
-    setJointNames(default_joints);
-
-    addLink("head_1", "neck_1");
-    addLink("neck_1", "left_shoulder_1");
-    addLink("left_shoulder_1", "left_elbow_1");
-    addLink("left_elbow_1", "left_hand_1");
-    addLink("neck_1", "right_shoulder_1");
-    addLink("right_shoulder_1", "right_elbow_1");
-    addLink("right_elbow_1", "right_hand_1");
-    addLink("neck_1", "torso_1");
-    addLink("torso_1", "left_hip_1");
-    addLink("left_hip_1", "left_knee_1");
-    addLink("left_knee_1", "left_foot_1");
-    addLink("torso_1", "right_hip_1");
-    addLink("right_hip_1", "right_knee_1");
-    addLink("right_knee_1", "right_foot_1");
-}
-
-void HumanMotionFeature::setJointNames(const std::vector<std::string>& joints)
-{
-    joint_names_ = joints;
-
-    map_joint_to_index_.clear();
-    streams_.resize(joints.size());
-    curves_.resize(joints.size());
-
-    for (int i=0; i<joints.size(); i++)
+    int num_links;
+    fscanf(fp, "%d", &num_links);
+    for (int i=0; i<num_links; i++)
     {
-        map_joint_to_index_[joints[i]] = i;
-        streams_[i].clear();
-        streams_[i].setCurveShape(num_pieces_, duration_);
+        fscanf(fp, "%s", name);
+        const int i0 = map_joint_to_index_[name];
+        fscanf(fp, "%s", name);
+        const int i1 = map_joint_to_index_[name];
+
+        links_.push_back(std::make_pair(i0, i1));
     }
-}
 
-void HumanMotionFeature::addLink(const std::string& joint1, const std::string& joint2)
-{
-    links_.push_back( std::make_pair( map_joint_to_index_[joint1], map_joint_to_index_[joint2] ) );
-}
+    fclose(fp);
 
-void HumanMotionFeature::setCurveShape(int num_pieces, double duration)
-{
-    num_pieces_ = num_pieces;
-    duration_ = duration;
-
-    for (int i=0; i<streams_.size(); i++)
-        streams_[i].setCurveShape(num_pieces, duration);
+    feature_.conservativeResize(joint_names_.size() * 3, 0);
 }
 
 void HumanMotionFeature::clear()
 {
-    for (int i=0; i<streams_.size(); i++)
-    {
-        streams_[i].clear();
-    }
+    joint_names_.clear();
+    map_joint_to_index_.clear();
+    links_.clear();
 }
 
-void HumanMotionFeature::observe(const std::string& joint_name, double time, const Eigen::Vector3d& joint_position)
+Eigen::MatrixXd HumanMotionFeature::feature()
 {
-    if (map_joint_to_index_.find(joint_name) != map_joint_to_index_.end())
-    {
-        const int index = map_joint_to_index_[joint_name];
-
-        streams_[index].push(time, joint_position);
-        streams_[index].fit();
-        curves_[index] = streams_[index].toHermiteCurve();
-    }
+    return feature_;
 }
 
-int HumanMotionFeature::featureSize(FeatureType feature_type)
+void HumanMotionFeature::addFrame(const Eigen::VectorXd& column)
 {
-    switch (feature_type)
-    {
-    case FEATURE_TYPE_ABSOLUTE_POSITION:
-        return featureSizeAbsolutePosition();
-
-    default:
-        fprintf(stderr, "Error: unknown feature type\n");
-        fflush(stderr);
-        return 3;
-    }
+    feature_.conservativeResize(Eigen::NoChange, feature_.cols() + 1);
+    feature_.col( feature_.cols() - 1 ) = column;
 }
 
-Eigen::VectorXd HumanMotionFeature::toFeature(FeatureType feature_type)
+void HumanMotionFeature::setVisualizerTopic(const std::string& topic)
 {
-    switch (feature_type)
-    {
-    case FEATURE_TYPE_ABSOLUTE_POSITION:
-        return toFeatureAbsolutePosition();
-
-    default:
-        fprintf(stderr, "Error: unknown feature type\n");
-        fflush(stderr);
-        return Eigen::Vector3d::Zero();
-    }
+    ros::NodeHandle nh;
+    publisher_.shutdown();
+    publisher_ = nh.advertise<visualization_msgs::MarkerArray>(topic, 100);
+    ros::Duration(1.0).sleep();
 }
 
-int HumanMotionFeature::featureSizeAbsolutePosition()
+void HumanMotionFeature::visualizeHumanMotion(const std::string& ns)
 {
-    const int num_curves = curves_.size();
-    const int feature_size = curves_[0].featureSize();
-
-    return num_curves * feature_size;
+    if (feature_.cols() > 0)
+        visualizeHumanMotion(0, feature_.cols(), ns);
 }
 
-Eigen::VectorXd HumanMotionFeature::toFeatureAbsolutePosition()
+void HumanMotionFeature::visualizeHumanMotion(int start, int end, const std::string& ns)
 {
-    const int num_curves = curves_.size();
-    const int feature_size = curves_[0].featureSize();
+    const double r = 0.025;
+    const double l = 0.01;
 
-    Eigen::VectorXd x( num_curves * feature_size );
+    std_msgs::ColorRGBA color0;
+    color0.r = 1;
+    color0.g = 1;
+    color0.b = 1;
+    color0.a = 1;
 
-    for (int i=0; i<curves_.size(); i++)
-        x.block(i * feature_size, 0, feature_size, 1) = curves_[i].toFeature();
+    std_msgs::ColorRGBA color1;
+    color1.r = 0;
+    color1.g = 0;
+    color1.b = 0;
+    color1.a = 1;
 
-    return x;
-}
-
-int HumanMotionFeature::encodingSize()
-{
-    int size = 0;
-
-    for (int i=0; i<curves_.size(); i++)
-        size += curves_[i].encodingSize();
-
-    return size;
-}
-
-Eigen::VectorXd HumanMotionFeature::encode()
-{
-    Eigen::VectorXd y;
-
-    for (int i=0; i<curves_.size(); i++)
-    {
-        Eigen::VectorXd cy = curves_[i].encode();
-        y.resize( y.rows() + cy.rows(), 1 );
-        y.bottomRows( cy.rows() ) = cy;
-    }
-
-    return y;
-}
-
-void HumanMotionFeature::decode(const Eigen::VectorXd& code)
-{
-    int p = 0;
-    for (int i=0; i<curves_.size(); i++)
-    {
-        curves_[i].decode( code.block(p, 0, p + (num_pieces_ + 1) * 6, 1) );
-        p += (num_pieces_ + 1) * 6;
-    }
-}
-
-void HumanMotionFeature::visualizeHumanMotion()
-{
     visualization_msgs::MarkerArray marker_array;
-
-    std_msgs::ColorRGBA white, black, red, green;
-    white.a = black.a = red.a = green.a = 1.;
-
-    white.r = white.g = white.b = 1.;
-    black.r = black.g = black.b = 0.;
-    red.r = 1.;
-    red.g = red.b = 0.;
-    green.g = 1.;
-    green.r = green.b = 0.;
-
-    appendTraceMarkers(0.025, white, black, marker_array);
-    appendJointMarkers(0, 0.05, white, marker_array);
-    appendJointMarkers(num_pieces_, 0.05, black, marker_array);
-    appendSkeletonMarkers(0, 0.025, white, marker_array);
-    appendSkeletonMarkers(num_pieces_, 0.025, black, marker_array);
-
-    publisher_.publish(marker_array);
-}
-
-void HumanMotionFeature::appendTraceMarkers(double size, const std_msgs::ColorRGBA& color0, const std_msgs::ColorRGBA& color1, visualization_msgs::MarkerArray& marker_array)
-{
-    const int res = 16;
-    const int num_points = num_pieces_ * res + 1;
-
     visualization_msgs::Marker marker;
-    std_msgs::ColorRGBA color;
 
-    marker.header.frame_id = "/world";
+    marker.header.frame_id = "world";
     marker.header.stamp = ros::Time();
-    marker.ns = "trace";
-    marker.type = visualization_msgs::Marker::LINE_STRIP;
     marker.action = visualization_msgs::Marker::ADD;
+    marker.ns = ns;
+    marker.id = 0;
     marker.pose.position.x = 0.;
     marker.pose.position.y = 0.;
     marker.pose.position.z = 0.;
@@ -241,118 +110,106 @@ void HumanMotionFeature::appendTraceMarkers(double size, const std_msgs::ColorRG
     marker.pose.orientation.x = 0.;
     marker.pose.orientation.y = 0.;
     marker.pose.orientation.z = 0.;
-    marker.scale.x = size;
 
-    for (int i=0; i < num_points; i++)
+    // joints
+    marker.type = visualization_msgs::Marker::SPHERE_LIST;
+    marker.scale.x = r * 2.;
+    marker.scale.y = r * 2.;
+    marker.scale.z = r * 2.;
+    for (int i=start; i<end; i++)
     {
-        const double t = (double)i / (num_points - 1);
+        const double t = (double)(i-start) / (end-start-1);
+        std_msgs::ColorRGBA color;
         color.r = (1-t) * color0.r + t * color1.r;
         color.g = (1-t) * color0.g + t * color1.g;
         color.b = (1-t) * color0.b + t * color1.b;
         color.a = (1-t) * color0.a + t * color1.a;
-        marker.colors.push_back(color);
-    }
 
-    for (int i=0; i<curves_.size(); i++)
-    {
-        marker.id = i;
-
-        marker.points.clear();
-
-        for (int j=0; j<num_points; j++)
+        for (int j=0; j<joint_names_.size(); j++)
         {
-            const double t = (double)j / (num_points - 1) * num_pieces_;
-            const Eigen::Vector3d x = curves_[i](t);
+            geometry_msgs::Point point;
+            point.x = feature_(3*j+0, i);
+            point.y = feature_(3*j+1, i);
+            point.z = feature_(3*j+2, i);
+            marker.points.push_back(point);
+            marker.colors.push_back(color);
+        }
+    }
+    marker_array.markers.push_back(marker);
+
+    // links
+    marker.colors.clear();
+    marker.points.clear();
+    marker.type = visualization_msgs::Marker::LINE_LIST;
+    marker.id = 1;
+    marker.scale.x = l;
+    for (int i=start; i<end; i++)
+    {
+        const double t = (double)(i-start) / (end-start-1);
+        std_msgs::ColorRGBA color;
+        color.r = (1-t) * color0.r + t * color1.r;
+        color.g = (1-t) * color0.g + t * color1.g;
+        color.b = (1-t) * color0.b + t * color1.b;
+        color.a = (1-t) * color0.a + t * color1.a;
+
+        for (int j=0; j<links_.size(); j++)
+        {
+            const int j0 = links_[j].first;
+            const int j1 = links_[j].second;
 
             geometry_msgs::Point point;
-            point.x = x(0);
-            point.y = x(1);
-            point.z = x(2);
+            point.x = feature_(3*j0+0, i);
+            point.y = feature_(3*j0+1, i);
+            point.z = feature_(3*j0+2, i);
             marker.points.push_back(point);
+            marker.colors.push_back(color);
+
+            point.x = feature_(3*j1+0, i);
+            point.y = feature_(3*j1+1, i);
+            point.z = feature_(3*j1+2, i);
+            marker.points.push_back(point);
+            marker.colors.push_back(color);
         }
-
-        marker_array.markers.push_back(marker);
     }
-}
-
-void HumanMotionFeature::appendJointMarkers(double time, double size, const std_msgs::ColorRGBA& color, visualization_msgs::MarkerArray& marker_array)
-{
-    visualization_msgs::Marker marker;
-
-    marker.header.frame_id = "/world";
-    marker.header.stamp = ros::Time();
-    marker.ns = "joints_" + std::to_string(time);
-    marker.type = visualization_msgs::Marker::SPHERE_LIST;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = 0.;
-    marker.pose.position.y = 0.;
-    marker.pose.position.z = 0.;
-    marker.pose.orientation.w = 1.;
-    marker.pose.orientation.x = 0.;
-    marker.pose.orientation.y = 0.;
-    marker.pose.orientation.z = 0.;
-    marker.scale.x = size;
-    marker.scale.y = size;
-    marker.scale.z = size;
-    marker.color = color;
-
-    for (int i=0; i<curves_.size(); i++)
-    {
-        marker.id = i;
-
-        const Eigen::Vector3d x = curves_[i](time);
-
-        geometry_msgs::Point point;
-        point.x = x(0);
-        point.y = x(1);
-        point.z = x(2);
-        marker.points.push_back(point);
-    }
-
     marker_array.markers.push_back(marker);
-}
 
-void HumanMotionFeature::appendSkeletonMarkers(double time, double size, const std_msgs::ColorRGBA& color, visualization_msgs::MarkerArray& marker_array)
-{
-    visualization_msgs::Marker marker;
-
-    marker.header.frame_id = "/world";
-    marker.header.stamp = ros::Time();
-    marker.ns = "skeleton_" + std::to_string(time);
-    marker.type = visualization_msgs::Marker::LINE_LIST;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = 0.;
-    marker.pose.position.y = 0.;
-    marker.pose.position.z = 0.;
-    marker.pose.orientation.w = 1.;
-    marker.pose.orientation.x = 0.;
-    marker.pose.orientation.y = 0.;
-    marker.pose.orientation.z = 0.;
-    marker.scale.x = size;
-    marker.scale.y = size;
-    marker.scale.z = size;
-    marker.color = color;
-
-    for (int i=0; i<links_.size(); i++)
+    // temporal links
+    marker.colors.clear();
+    marker.points.clear();
+    marker.id = 2;
+    for (int i=start; i<end-1; i++)
     {
-        marker.id = i;
+        const double t0 = (double)(i-start) / (end-start-1);
+        std_msgs::ColorRGBA colort0;
+        colort0.r = (1-t0) * color0.r + t0 * color1.r;
+        colort0.g = (1-t0) * color0.g + t0 * color1.g;
+        colort0.b = (1-t0) * color0.b + t0 * color1.b;
+        colort0.a = (1-t0) * color0.a + t0 * color1.a;
 
-        const int i0 = links_[i].first;
-        const int i1 = links_[i].second;
+        const double t1 = (double)(i-start) / (end-start-1);
+        std_msgs::ColorRGBA colort1;
+        colort1.r = (1-t1) * color0.r + t1 * color1.r;
+        colort1.g = (1-t1) * color0.g + t1 * color1.g;
+        colort1.b = (1-t1) * color0.b + t1 * color1.b;
+        colort1.a = (1-t1) * color0.a + t1 * color1.a;
 
-        const Eigen::Vector3d x0 = curves_[i0](time);
-        const Eigen::Vector3d x1 = curves_[i1](time);
+        for (int j=0; j<joint_names_.size(); j++)
+        {
+            geometry_msgs::Point point;
+            point.x = feature_(3*j+0, i);
+            point.y = feature_(3*j+1, i);
+            point.z = feature_(3*j+2, i);
+            marker.points.push_back(point);
+            marker.colors.push_back(colort0);
 
-        geometry_msgs::Point point;
-        point.x = x0(0);
-        point.y = x0(1);
-        point.z = x0(2);
-        marker.points.push_back(point);
-        point.x = x1(0);
-        point.y = x1(1);
-        point.z = x1(2);
-        marker.points.push_back(point);
+            point.x = feature_(3*j+0, i+1);
+            point.y = feature_(3*j+1, i+1);
+            point.z = feature_(3*j+2, i+1);
+            marker.points.push_back(point);
+            marker.colors.push_back(colort1);
+        }
     }
-
     marker_array.markers.push_back(marker);
+
+    publisher_.publish(marker_array);
 }
